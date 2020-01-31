427c2808ea4d317d1c7e0aa6a21b7d01abbc40b7
Andrew Rodriguez <arodriguez2@ucvts.org>
2020-01-31 14:10:56 -0500

    Fifteenth Commit

--- src/Exercises.java
+++ src/Exercises.java
@@ -33,7 +33,7 @@ public class Exercises {
 	public ArrayList<String> middle(ArrayList<String> values) {
 		// write your code here
 		
-		return null;	// sssssssssssssdefault return value to ensure compilation
+		return null;	// default return value to ensure compilation
 	}
 
 	public boolean increasing(ArrayList<Integer> numbers) {
